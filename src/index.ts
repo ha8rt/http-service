edefa0342326c12c13946b3d0ba8244b45842570
Tamás Varró <varro.tamas@szegedsw.hu>
2019-08-09 17:04:21 +0200

    1.0.2

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export * from "./body/body";
+// export * from "./config/config";
+export * from "./http.service/http.service";
+// export * from "./service/obs";
+export * from "./service/query";
+export * from "./service/service";
